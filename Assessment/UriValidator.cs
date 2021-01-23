e9906e423f46705540f5b5021bd5a75bda865cb4
Chularansi Fernando <chularansifernando@yahoo.com>
2021-01-23 13:14:40 +0100

    Done Validate method in UriValidator class.

--- Assessment/UriValidator.cs
+++ Assessment/UriValidator.cs
@@ -1,11 +1,48 @@
 ﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Assessment
 {
     public class UriValidator
     {
-        
+        public bool Validate(string uri, IEnumerable<string> rules)
+        {
+            bool result = false;
+            Regex rgx;
+
+            foreach (string rule in rules)
+            {
+                if (rule.StartsWith('@'))
+                {
+                    string pattern = rule[1..]; // simplified method for rule.Substring(1)
+
+                    rgx = new Regex(pattern);
+                    result = rgx.IsMatch(uri);
+                }
+                else if (rule.Contains('*'))
+                {
+                    string pattern = "^";
+                    string[] operands = Regex.Split(rule, @"\*");
+
+                    for (int i = 0; i < operands.Length - 1; i++)
+                    {
+                        pattern += operands[i] + "[a-z]+";
+                    }
+                    pattern += operands[^1]; // simplified method for operands.Length - 1
+
+                    rgx = new Regex(pattern);
+                    result = rgx.IsMatch(uri);
+                }
+                else
+                {
+                    if (uri.ToLower() == rule.ToLower())
+                    {
+                        result = true;
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
