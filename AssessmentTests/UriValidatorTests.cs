affe7888b10dc4fd125a1460d9c6cb8635a81eeb
Chularansi Fernando <chularansifernando@yahoo.com>
2021-01-23 13:35:31 +0100

    Test Wildcard Rule.

--- AssessmentTests/UriValidatorTests.cs
+++ AssessmentTests/UriValidatorTests.cs
@@ -31,5 +31,44 @@ namespace AssessmentTests
             var result = validator.Validate(@"https://www.google.com/webapp/logincallback/", googleRules);
             Assert.AreEqual(result, false);
         }
+
+        [TestMethod]
+        public void WildcardRuleMethod_No1()
+        {
+            UriValidator validator = new UriValidator();
+            string[] githubRules = new[]{
+                "https://*.github.com/*/logincallback", // wildcard rule
+	            "https://*.github.com/webapp/login" // wildcard rule
+            };
+
+            var result = validator.Validate(@"https://www.github.com/webapp/login", githubRules);
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void WildcardRuleMethod_No2()
+        {
+            UriValidator validator = new UriValidator();
+            string[] confirmitRules = new[]{
+                "https://*.confirmit.com/*/logincallback" // wildcard rule
+            };
+
+            var result = validator.Validate(@"https://app.confirmit.com/auth/folder/logincallback", confirmitRules);
+            Assert.AreEqual(result, false);
+        }
+
+        [TestMethod]
+        public void WildcardRuleMethod_No3()
+        {
+            UriValidator validator = new UriValidator();
+            string[] confirmitRules = new[]{
+                "https://*.confirmit.com/*/logincallback" // wildcard rule
+            };
+
+            var result = validator.Validate(@"https://confirmit.com/logincallback", confirmitRules);
+            Assert.AreEqual(result, false);
+        }
+
+        
     }
 }
