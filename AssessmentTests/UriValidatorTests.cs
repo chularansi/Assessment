c61edf2190ec98a3ad3c2d2e8210a2b078b36e76
Chularansi Fernando <chularansifernando@yahoo.com>
2021-01-23 13:33:25 +0100

    Test Absolute Rule.

--- AssessmentTests/UriValidatorTests.cs
+++ AssessmentTests/UriValidatorTests.cs
@@ -8,6 +8,28 @@ namespace AssessmentTests
     [TestClass]
     public class UriValidatorTests
     {
-        
+        [TestMethod]
+        public void AbsoluteRuleMethod_No1()
+        {
+            UriValidator validator = new UriValidator();
+            string[] googleRules = new[]{
+                "https://www.google.com/webapp/logincallback" // absolute rule
+            };
+
+            var result = validator.Validate(@"https://www.google.com/webapp/logincallback", googleRules);
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void AbsoluteRuleMethod_No2()
+        {
+            UriValidator validator = new UriValidator();
+            string[] googleRules = new[]{
+                "https://www.google.com/webapp/logincallback" // absolute rule
+            };
+
+            var result = validator.Validate(@"https://www.google.com/webapp/logincallback/", googleRules);
+            Assert.AreEqual(result, false);
+        }
     }
 }
