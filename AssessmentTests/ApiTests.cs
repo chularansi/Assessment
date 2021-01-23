79f360b05e8c5c047b02a06b4e6cbd053e0919a6
Chularansi Fernando <chularansifernando@yahoo.com>
2021-01-23 21:52:38 +0100

    Check with correct url. Result Success.

--- AssessmentTests/ApiTests.cs
+++ AssessmentTests/ApiTests.cs
@@ -1,27 +1,38 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Api;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace AssessmentTests
 {
     [TestClass]
     public class ApiTests
     {
+        static WebApplicationFactory<Startup> webApplicationFactory;
+        static HttpClient httpClient;
+
+        readonly string baseUrl = "http://localhost:65179";
+
+        [ClassInitialize]
+        public static async Task ClassStartup(TestContext context)
+        {
+            webApplicationFactory = new WebApplicationFactory<Startup>();
+            httpClient = webApplicationFactory.CreateClient();
+        }
+
         [TestMethod]
         public async Task CallUnsecureApi_NoAuth_Ok()
         {
-            // Arrange
-            /*
-             * use TestServer class to create a client and call "api/values"
-             */
+            string getValues = $"{baseUrl}/api/values";
             var expected = HttpStatusCode.OK;
 
-            // Act
+            var response = await httpClient.GetAsync(getValues);
 
-            // uncomment the line below
-            // var response = await client.GetAsync(url);
+            Assert.AreEqual(expected, response.StatusCode);
 
-            // Assert that response.StatusCode matches expectedStatus
+            httpClient.Dispose();
         }
 
         [TestMethod]
