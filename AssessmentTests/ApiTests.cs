e95967ab712f0b98f76f04135418c63ca2be2b85
Chularansi Fernando <chularansifernando@yahoo.com>
2021-01-24 21:07:16 +0100

    Test Secure Api with fake token. Got Assert.AreEqual failed. Expected:<OK>. Actual:<Unauthorized>.

--- AssessmentTests/ApiTests.cs
+++ AssessmentTests/ApiTests.cs
@@ -66,5 +66,21 @@ namespace AssessmentTests
 
             httpClient.Dispose();
         }
+
+        [TestMethod]
+        public async Task CallSecureApiRoot_WithFakeToken_Response()
+        {
+            var token = "yJhbGciOiJSUzI1NiIsImtpZCI6IjVDQ0FBMDNFRERFMjZENTMxMDRDQzM1RDBENEIyOTlDIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2MTE1MTUxMzQsImV4cCI6MTYxMTUxODczNCwiaXNzIjoiaHR0cHM6Ly9kZW1vLmlkZW50aXR5c2VydmVyLmlvIiwiYXVkIjoiYXBpIiwiY2xpZW50X2lkIjoibTJtIiwianRpIjoiRjkyQzk1MUMzQjczN0E3NkY5OTNFODc0NTE3OTlGQUUiLCJpYXQiOjE2MTE1MTUxMzQsInNjb3BlIjpbImFwaSJdfQ.FDQqH5PzFe_yg102wKNwSIoUhAswsVc7dygTpyTXe7FpmHfuXtposmwZdxPubcyXnjsYndSp5DeJSvlzSiMqpnC_HP-Ybp24SohjAi2FPynTafn7FeAKIHua2-XZMTNakZM8A-SPLr11XP_gWC72psoEjl5iMUIHWkGr_2kTJYPfRygHAD-J-zV3oapFssesbTMVfwwJJI7b3CzvkmmdpFAc-vLsIIpMWnxrACTrFC6XZfCagAa8Dvt9Sat-VrsrEEzqIDviqbvpdmsRE2XxAUpvqBXPDqs2htgFs8p1KcoVa1PAwzno326wmN0Kw6XOh_7iOkGBuODzdBotc6HtBQ";
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            string getValues = $"{baseUrl}/api/securevalues";
+            var expected = HttpStatusCode.OK;
+
+            var response = await httpClient.GetAsync(getValues);
+
+            Assert.AreEqual(expected, response.StatusCode);
+
+            httpClient.Dispose();
+        }
     }
 }
